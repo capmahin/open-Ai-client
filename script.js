29bb9980d01947e6c400d74bf52fef86635960f5
capmahin <mahinulhuque1996@gmail.com>
2024-09-21 00:32:24 +0600

    add a element

--- script.js
+++ script.js
@@ -23,7 +23,8 @@ function typeText(element, text){
 
     let interval = setInterval(()=>{
        if(index < text.length){
-        
+        element.innerHTML += text.chartAt(index);
+        index++;
        }
     },20)
 }
