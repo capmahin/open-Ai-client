cc9261540781ac0da1e13b42cc4722bbb72f5005
capmahin <mahinulhuque1996@gmail.com>
2024-09-20 23:02:07 +0600

    add loaderinterval

--- script.js
+++ script.js
@@ -2,4 +2,10 @@ import bot from './assets/bot.svg';
 import user from './assets/user.svg';
 
 const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container')
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element){
+    
+}
