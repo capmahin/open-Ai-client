d3d363d2485b57dbe09c4a79d6efb6e6d6edea10
capmahin <mahinulhuque1996@gmail.com>
2024-09-21 22:00:25 +0600

    add chatcontainer

--- script.js
+++ script.js
@@ -77,4 +77,6 @@ const handleSubmit = async (e)=>{
 
     const uniqueId = generateUniqueId();
     chatContainer.innerHTML += chatStripe(true," ", uniqueId);
+
+    chatContainer.scrollTop = chatContainer.scrollHeight;
 }
